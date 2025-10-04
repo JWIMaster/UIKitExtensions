fad9af306278cf443453fd4155fa21549fa634cf
JWI <jwi@j-w-i.org>
2025-10-04 21:36:54 +1000

    add AddAction, will be changing later

--- Sources/UIKitExtensions/UIButton.swift
+++ Sources/UIKitExtensions/UIButton.swift
@@ -69,3 +69,20 @@ extension UIButton {
     }
 }
 
+private class ClosureSleeve {
+    let closure: () -> Void
+    init(_ closure: @escaping () -> Void) {
+        self.closure = closure
+    }
+    @objc func invoke() { closure() }
+}
+
+extension UIButton {
+    func addAction(for controlEvents: UIControl.Event, _ closure: @escaping () -> Void) {
+        let sleeve = ClosureSleeve(closure)
+        addTarget(sleeve, action: #selector(ClosureSleeve.invoke), for: controlEvents)
+        // Use NSUUID for unique key
+        objc_setAssociatedObject(self, NSUUID().uuidString, sleeve, .OBJC_ASSOCIATION_RETAIN)
+    }
+}
+
