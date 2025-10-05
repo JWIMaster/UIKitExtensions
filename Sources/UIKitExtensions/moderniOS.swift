0adaf121e1067007ec882ce0f9a20f20a817f9cc
JWI <jwi@j-w-i.org>
2025-10-05 17:06:28 +1100

    a

--- Sources/UIKitExtensions/moderniOS.swift
+++ Sources/UIKitExtensions/moderniOS.swift
@@ -10,7 +10,7 @@ import UIKit
 import UIKitCompatKit
 
 extension UINavigationController {
-    public func moderniOS() {
+    public func moderniOSNavBar() {
         self.navigationBar.backgroundColor = .white
         self.navigationBar.setBackgroundImage(UIImage(), for: .default)
         self.navigationBar.shadowImage = UIImage()
@@ -51,25 +51,5 @@ extension UIView {
             self.addSubview(statusBar)
             self.bringSubviewToFront(statusBar)
         }
-    }
-}
-
-extension UIWindow {
-    public func moderniOS(backgroundColor: UIColor = .white) {
-        let statusBarFrame: CGRect
-        if #available(iOS 13.0, *) {
-            statusBarFrame = self.windowScene?.statusBarManager?.statusBarFrame ?? CGRect(x: 0, y: 0, width: self.bounds.width, height: 20)
-        } else {
-            statusBarFrame = UIApplication.shared.statusBarFrame
-        }
-        
-        
-        let statusBarBackground = UIView(frame: statusBarFrame)
-        statusBarBackground.backgroundColor = backgroundColor
-        statusBarBackground.autoresizingMask = [.flexibleWidth, .flexibleBottomMargin]
-        statusBarBackground.isUserInteractionEnabled = false
-        
-        self.addSubview(statusBarBackground)
-        self.bringSubviewToFront(statusBarBackground)
     }
 }
