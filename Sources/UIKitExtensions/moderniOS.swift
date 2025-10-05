59fe549befecdb53f5149f11cc19b0a927b41bc3
JWI <jwi@j-w-i.org>
2025-10-05 13:33:14 +1100

    a

---
+++ Sources/UIKitExtensions/moderniOS.swift
@@ -0,0 +1,63 @@
+//
+//  File.swift
+//  
+//
+//  Created by JWI on 5/10/2025.
+//
+
+import Foundation
+import UIKit
+
+
+extension UINavigationController {
+    public func moderniOS() {
+        self.navigationBar.backgroundColor = .white
+        self.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        self.navigationBar.shadowImage = UIImage()
+        self.navigationBar.tintColor = .white
+    }
+}
+
+extension UIView {
+    public func moderniOSStatusBar(backgroundColor: UIColor = .white) {
+        let statusBarFrame: CGRect
+        if #available(iOS 13.0, *) {
+            if let windowScene = self.window?.windowScene,
+               let frame = windowScene.statusBarManager?.statusBarFrame {
+                statusBarFrame = frame
+            } else {
+                statusBarFrame = CGRect(x: 0, y: 0, width: self.bounds.width, height: UIApplication.shared.statusBarFrame.height)
+            }
+        } else {
+            statusBarFrame = UIApplication.shared.statusBarFrame
+        }
+
+        let statusBarBackground = UIView(frame: statusBarFrame)
+        statusBarBackground.backgroundColor = backgroundColor
+        statusBarBackground.autoresizingMask = [.flexibleWidth, .flexibleBottomMargin]
+        statusBarBackground.isUserInteractionEnabled = false
+
+        self.addSubview(statusBarBackground)
+        self.bringSubviewToFront(statusBarBackground)
+    }
+}
+
+extension UIWindow {
+    public func moderniOS(backgroundColor: UIColor = .white) {
+        let statusBarFrame: CGRect
+        if #available(iOS 13.0, *) {
+            statusBarFrame = self.windowScene?.statusBarManager?.statusBarFrame ?? CGRect(x: 0, y: 0, width: self.bounds.width, height: 20)
+        } else {
+            statusBarFrame = UIApplication.shared.statusBarFrame
+        }
+        
+        
+        let statusBarBackground = UIView(frame: statusBarFrame)
+        statusBarBackground.backgroundColor = backgroundColor
+        statusBarBackground.autoresizingMask = [.flexibleWidth, .flexibleBottomMargin]
+        statusBarBackground.isUserInteractionEnabled = false
+
+        self.addSubview(statusBarBackground)
+        self.bringSubviewToFront(statusBarBackground)
+    }
+}
