e3ad8aea4b2e370044f1009d213a0e1a632f7ea5
JWI <jwi@j-w-i.org>
2025-10-15 08:37:44 +1100

    a

--- Sources/UIKitExtensions/UIColor.swift
+++ Sources/UIKitExtensions/UIColor.swift
@@ -9,7 +9,7 @@ import Foundation
 import UIKit
 
 public extension UIColor {
-    class var randomColor: UIColor {
+    class var random: UIColor {
         return UIColor.init(red: .random(in: 0...1), green: .random(in: 0...1), blue: .random(in: 0...1), alpha: 1)
     }
 }
