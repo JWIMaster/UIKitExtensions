d4c3ccae77590edda20a913903c1b7b26f786552
JWI <jwi@j-w-i.org>
2025-10-15 08:36:07 +1100

    Add randomcolor

---
+++ Sources/UIKitExtensions/UIColor.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by JWI on 15/10/2025.
+//
+
+import Foundation
+import UIKit
+
+public extension UIColor {
+    var randomColor: UIColor {
+        return UIColor.init(red: .random(in: 0...1), green: .random(in: 0...1), blue: .random(in: 0...1), alpha: 1)
+    }
+}
