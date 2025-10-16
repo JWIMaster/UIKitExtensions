28bb31f3cd5b872c76dee24151710f10a032633c
JWI <jwi@j-w-i.org>
2025-10-16 15:48:26 +1100

    a

--- Package.swift
+++ Package.swift
@@ -22,8 +22,13 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "UIKitExtensions",
-            dependencies: ["UIKitCompatKit"],
+            dependencies: ["UIKitCompatKit", "RealtimeBlur"],
             path: "Sources/UIKitExtensions"),
+        .target(
+            name: "RealtimeBlur",
+            path: "Sources/RealtimeBlur",
+            publicHeadersPath: "."
+        ),
         .testTarget(
             name: "UIKitExtensionsTests",
             dependencies: ["UIKitExtensions"]),
