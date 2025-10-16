31797178370be967f9857b83fca39b34cb1ed20f
JWI <jwi@j-w-i.org>
2025-10-16 15:08:39 +1100

    a

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "UIKitExtensions",
-            dependencies: ["UIKitCompatKit"],
+            dependencies: ["UIKitCompatKit", "FXBlurView"],
             path: "Sources/UIKitExtensions"),
         .target(
             name: "FXBlurView",
