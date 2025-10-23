7f6aa3fe3c173370150b6688a6c62a54e9bbfed5
JWI <jwi@j-w-i.org>
2025-10-23 12:39:00 +1100

    a

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "UIKitExtensions",
-            dependencies: ["UIKitCompatKit", "FoundationCompatKit"],
+            dependencies: ["UIKitCompatKit", "FoundationCompatKit", "iOS6BarFix"],
             path: "Sources/UIKitExtensions"),
         .target(
             name: "iOS6BarFix",
