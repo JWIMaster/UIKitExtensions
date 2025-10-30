abe9041212d13d7120f4341c274492640509d271
JWI <jwi@j-w-i.org>
2025-10-30 16:32:50 +1100

    a

--- Sources/UIKitExtensions/LiquidGlassView.swift
+++ Sources/UIKitExtensions/LiquidGlassView.swift
@@ -282,8 +282,10 @@ public class LiquidGlassView: UIView {
         layer.shadowOpacity = shadowOpacity
         layer.shadowRadius = shadowRadius
         layer.shadowOffset = shadowOffset
-        //layer.shouldRasterize = true
-        layer.rasterizationScale = UIScreen.main.scale
+        if #unavailable(iOS 8.0) {
+            layer.shouldRasterize = true
+            layer.rasterizationScale = UIScreen.main.scale
+        }
 
         solidView?.layer.cornerRadius = cornerRadius
         blurView?.layer.cornerRadius = cornerRadius
