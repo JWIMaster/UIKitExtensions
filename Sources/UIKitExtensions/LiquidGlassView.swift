edd5ed698728eea6f940df48a6d85918ef5f7ae5
JWI <jwi@j-w-i.org>
2025-11-24 17:07:20 +1100

    make liquidglassview filter options conform to caseiterable

--- Sources/UIKitExtensions/LiquidGlassView.swift
+++ Sources/UIKitExtensions/LiquidGlassView.swift
@@ -70,7 +70,7 @@ public class LiquidGlassView: UIView {
         }
     }
 
-    public enum AdvancedFilterOptions {
+    public enum AdvancedFilterOptions: CaseIterable {
         case tint, darken, highlight, depth, rim, innerShadow
     }
 
